1f51d718712e810d2500d9d20eabd11b4ed6533f
hzh7 <hzh_aj@163.com>
2016-10-09 20:07:57 +0800

    o.o

--- 234. Palindrome Linked List.py
+++ 234. Palindrome Linked List.py
@@ -1,14 +1,15 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 
 class Solution(object):
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
+        """
         """
         if not head:
             return True
@@ -20,6 +21,29 @@ class Solution(object):
             else:
                 return False
         return True
-a = ["a","g","s","s","g","a"]
+        """
+        if head == None or head.next == None:
+            return True
+        ptr1, ptr2, ptr3 = head, head, head
+        while ptr1.next and ptr2.next.next:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next.next
+        r, p = ptr1.next, ptr2
+        while r == p:
+            while r.next == p:
+                r.next = p
+                r.next = None
+                p.next = r
+                p = r
+                r = ptr1.next
+            r == r.next
+        ptr1.next = None
+        while ptr3.next:
+            if ptr3.val != ptr2.val:
+                return False
+            ptr3 = ptr3.next
+            ptr2 = ptr2.next
+        return True
+a = ListNode([1,2])
 b = Solution().isPalindrome(a)
 print b
