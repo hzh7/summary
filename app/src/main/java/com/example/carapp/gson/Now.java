e0d92e1649a0d6af6f24f5a31eecad3fc4979c3f
hzh7 <hzh_aj@163.com>
2017-02-24 18:13:48 +0800

    添加了天气

---
+++ app/src/main/java/com/example/carapp/gson/Now.java
@@ -0,0 +1,20 @@
+package com.example.carapp.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by HZH on 2017/2/23.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public  class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
